2a5329146283ff258c8cc03944f9e2115463704c
bernd7055 <b9772647@gmail.com>
2025-08-10 17:56:07 +0200

    create program to move a window to a new workspace on second monitor

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/lasfo/i3tools
+
+go 1.24.5
+
+require (
+	github.com/BurntSushi/xgb v0.0.0-20160522181843-27f122750802 // indirect
+	github.com/BurntSushi/xgbutil v0.0.0-20160919175755-f7c97cef3b4e // indirect
+	go.i3wm.org/i3 v0.0.0-20190720062127-36e6ec85cc5a // indirect
+)
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"go.i3wm.org/i3"
+)
+
+func main() {
+	// Create a new workspace on the second contain and move the currently
+	// focused container there.
+
+	wss, err := i3.GetWorkspaces()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var nextWS int64
+	for _, ws := range wss {
+		nextWS = max(nextWS, ws.Num)
+	}
+	nextWS++
+
+	tree, err := i3.GetTree()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	focusedCon := tree.Root.FindFocused(func(n *i3.Node) bool {
+		return n.Focused
+	})
+	fmt.Printf("%+#v\n", focusedCon)
+	i3.RunCommand(fmt.Sprintf(`workspace %[1]d; move workspace to output HDMI-0; [con_id="%[2]d"] focus; move container to workspace number %[1]d`, nextWS, focusedCon.ID))
+}
