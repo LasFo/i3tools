9bb6eac950417526be33b04a40f6b0e216a15c53
bernd7055 <b9772647@gmail.com>
2025-08-10 18:03:02 +0200

    add comment to explain hard coding of output

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 	focusedCon := tree.Root.FindFocused(func(n *i3.Node) bool {
 		return n.Focused
 	})
-	fmt.Printf("%+#v\n", focusedCon)
+	// Hard coding the output for now because it always only want to move to
+	// my secondary monitor. Never to my primary monitor.
 	i3.RunCommand(fmt.Sprintf(`workspace %[1]d; move workspace to output HDMI-0; [con_id="%[2]d"] focus; move container to workspace number %[1]d`, nextWS, focusedCon.ID))
 }
